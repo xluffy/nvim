3af1c5256fa953f2d34b8abf1ce925d332319444
xluffy <quang@2meo.com>
2023-04-19 18:03:05 +0700

    Update user config

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua", "bash", "c", "dockerfile", "go", "hcl", "json", "python", "ruby", "rust", "sql", "terraform", "vim", "yaml" },
   },
 }
